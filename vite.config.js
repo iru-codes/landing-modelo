adf964faad9a469a9cbebbb32232f49a16a73678
iru-codes <aixairupe992@gmail.com>
2025-01-26 15:23:01 -0300

    Ajustes vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: 'public', // Salida en la carpeta 'public' para Firebase Hosting
-  },
-});
+    root: './public', // Usa 'public' como carpeta raíz del proyecto
+    build: {
+      outDir: '../dist', // Salida de la build en una carpeta separada ('dist')
+      emptyOutDir: true, // Limpia la carpeta de destino antes de generar la build
+    },
+    server: {
+      open: true, // Abre el navegador automáticamente al iniciar el servidor de desarrollo
+    },
+  });
